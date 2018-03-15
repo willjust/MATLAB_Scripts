8f731c01504d4b75b34c15a300f729171008a517
Justin Williams <justin.williams@wisc.edu>
2018-03-14 23:14:23 -0500

    Finished problems 3/6

--- MatrixSolvers/Solve_QR.m
+++ MatrixSolvers/Solve_QR.m
@@ -1,7 +1,30 @@
 function [x] = Solve_QR(A, b)
+[m, n] = size(A);
+R = A;
+for i = 1:n
+ x = R(:,i);
+ y = [x(1:i-1)' norm(x(i:m)) zeros(1, m-i)]';
+ w = x-y;
+ if norm(w) ~= 0
+ w = w/norm(w);
+ end
+ R = R - 2*w*(w'*R);
+ b = b - 2*w*(w'*b);
+end
+R
+x = zeros(n,1);
+for i = n:-1:1
+ x(i) = (b(i) - (R(i,i+1:end)*x(i+1:end)))/R(i,i);
+end
+
 %% Solves linear system Ax = b
 %	Uses an efficient QR-Factorization scheme to find x = A\b
 %	Never explicitly calculates Q, making algorithm fast
-[R, v] = qr_custom(A);
-x = QTX(v, b);
-x = R\x;
+% [m, ~] = size(A);
+% [R, v] = qr_custom(A);
+% x = QTX(v, b);
+% R
+% for i = m-1:-1:1
+%  x(i) = (x(i) - ...
+% 	 (R(i,i+1:end)*x(i+1:end)))/R(i,i);
+% end
